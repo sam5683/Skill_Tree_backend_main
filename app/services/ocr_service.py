d3305fb190ed5b7243f45561ea8aff7b57a7824d
sam5683 <sameerkaushal35@gmail.com>
2026-04-24 16:03:13 +0530

    tesseract ocr service layer update

--- app/services/ocr_service.py
+++ app/services/ocr_service.py
@@ -1,24 +1,33 @@
 import pytesseract
 from PIL import Image
-from fastapi import UploadFile
+from fastapi import UploadFile, HTTPException
 import io
-from fastapi import HTTPException
+import os
+import subprocess
+
 pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 
 async def extract_text_from_image(file: UploadFile) -> str:
     try:
+
+        print("PATH CHECK:", os.path.exists("/usr/bin/tesseract"))
+
+        try:
+            version = subprocess.check_output(["tesseract", "--version"]).decode()
+            print("TESSERACT VERSION:", version)
+        except Exception as e:
+            print("TESSERACT EXEC ERROR:", str(e))
+
         contents = await file.read()
+        print("FILE SIZE:", len(contents))
 
         image = Image.open(io.BytesIO(contents))
 
         text = pytesseract.image_to_string(image)
 
-       # minimal cleanup
         lines = [line.strip() for line in text.split("\n") if line.strip()]
-        clean_text = "\n".join(lines[:275]) # limit to first 275 lines
-
-        return clean_text
+        return "\n".join(lines[:275])
 
     except Exception as e:
-        print("OCR ERROR:", str(e)) 
+        print("OCR ERROR:", str(e))
         raise HTTPException(status_code=500, detail=str(e))
